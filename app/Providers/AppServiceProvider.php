a810abbd88a4c72aa3c95a2a9f7f9377d3d62db1
Arif Khan <arifkhan.incaendo@gmail.com>
2018-01-24 21:49:39 +0530

    Optimize channel query

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
     {
         \View::composer('*', function ($view) {
             $channels = \Cache::rememberForever('channels', function () {
-                return Channel::all();
+                return Channel::withCount('threads')->get();
             });
 
             $view->with('channels', $channels);
